cc625ef43d1d025dea3936e0f436707d3c3b4933
EMAKINA\msr <msr@emakina.com>
2022-02-09 22:48:20 +0100

    Need to create request body and add code to handle posting to rest api.

--- csharp/RabbitReader/RabbitReader/Program.cs
+++ csharp/RabbitReader/RabbitReader/Program.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Configuration;
 using RabbitMQ.Client.Events;
 using RabbitReader;
 using RabbitReader.RabbitMQ;
 using System.Text;
+using System.Text.Json;
 
 IConfiguration config = Startup.BuildConfiguration();
 Startup.ConfigureLogger();
+HttpClient client = new HttpClient();
 
 
 var applicationSettings = config.GetSection("ApplicationSettings");
@@ -20,7 +23,9 @@ Console.ReadLine();
 
 static void OnMessageReceived(object? model, BasicDeliverEventArgs ea)
 {
+    //ToDo follow acordingly: https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/console-webapiclient
     var body = ea.Body.ToArray();
     var message = Encoding.UTF8.GetString(body);
+    var serializedResponse = JsonSerializer.Serialize(body);
     Console.WriteLine("Received {0} at {1}", message, DateTime.Now);
 }
