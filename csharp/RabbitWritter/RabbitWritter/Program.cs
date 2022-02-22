19d880bb8152da7d66fbcc5ffc2dd0b5f9e98017
EMAKINA\msr <msr@emakina.com>
2022-02-22 22:36:51 +0100

    Further refactoring required. Need to implement the logic for writing to the queue.

--- csharp/RabbitWritter/RabbitWritter/Program.cs
+++ csharp/RabbitWritter/RabbitWritter/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Core.Library.RabbitMQ;
+using RabbitWriter;
+using Core.Library.RabbitMQ;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(Startup.BuildConfiguration)
+    .ConfigureLogging(x => x.ConfigureLogger())
+    .ConfigureServices((_, services) => Startup.ConfigureServices(services))
+    .Build();
+
+var queueWriter = host.Services.GetService<IQueueWriterDeclaration>();
