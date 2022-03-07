c6344ad4266448d68a4f0184b227e23d2f87a4ae
EMAKINA\msr <msr@emakina.com>
2022-03-07 19:14:43 +0100

    Added couchbase packages.

--- csharp/SensorDataStore/SensorDataStore.WebApi/Api.cs
+++ csharp/SensorDataStore/SensorDataStore.WebApi/Api.cs
@@ -1,15 +1,25 @@
-﻿namespace SensorDataStore.WebApi
+﻿using Core.Library.Models;
+
+namespace SensorDataStore.WebApi
 {
     public static class Api
     {
-        public static void ConfigureApi()
+        public static void ConfigureApi(this WebApplication app)
+        {
+            app.MapGet("/BMPSensor", Get);
+            app.MapPost("/BMPSensor", Post);
+        }
+
+        private static async Task<IEnumerable<BmpMeasurementDto>> Get()
         {
 
         }
 
-        public static async Task PostAsync()
+        private static async Task Post()
         {
 
         }
+
+
     }
 }
