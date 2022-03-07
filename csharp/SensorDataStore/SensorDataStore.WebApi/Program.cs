172ae811d089b99adb4231507131d58f334a71fb
EMAKINA\msr <msr@emakina.com>
2022-03-07 21:30:15 +0100

    Data store returns sample data.
    Added swagger.

--- csharp/SensorDataStore/SensorDataStore.WebApi/Program.cs
+++ csharp/SensorDataStore/SensorDataStore.WebApi/Program.cs
@@ -1,6 +1,24 @@
+using SensorDataStore.WebApi;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddAuthorization();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.ConfigureServices();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.ConfigureApi();
 
 app.Run();
